ae06b0da5179bb8e948a81ef2de5ebbcf48d9405
Fidesnoella <fniragena@gmail.com>
2022-11-23 12:47:59 +0200

    ft/ensure code consistency

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -7,16 +7,12 @@ import arrow from '../../public/assets/images/arrow.svg'
 function Button({children, style, link}) {
     return (
         <Wrapper>
-            <div>
                 <Link href={`${link}`}>
-                <div className='flex gap-1 items-center group cursor-pointer'>
-                <span className={`text-2xl font-normal leading-8 tracking-thin font-sans ${style}`}>{children}</span>
-                <span className='group-hover:-translate-y-1 transition-transform duration-300 group-hover:translate-x-1'><Image src={arrow} /></span>
-                </div>
-                
+                   <div className='flex gap-1 items-center group cursor-pointer'>
+                   <span className={`text-2xl font-normal leading-8 tracking-thin font-sans ${style}`}>{children}</span>
+                   <span className='group-hover:-translate-y-1 transition-transform duration-300 group-hover:translate-x-1'><Image src={arrow} /></span>
+                   </div>
                 </Link>
-                
-            </div>
         </Wrapper>  
     );
 }
