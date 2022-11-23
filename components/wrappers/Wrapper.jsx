e75614ba51857ddb858f196b49d04aa77a928807
iamzilfa <iamzilfa@gmail.com>
2022-11-23 14:55:40 +0200

    feat: adding header section

--- components/wrappers/Wrapper.jsx
+++ components/wrappers/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Wrapper({children,styles}) {
     return (
-        <div className={`max-w-82 px-16 mx-auto ${styles}`} >
+        <div className={`max-w-82 md:px-16 px-5 mx-auto ${styles}`} >
            {children} 
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,18 @@ module.exports = {
           100: "#9A9A9A",
         },
       },
-     fontFamily: {
-        'sans' : ['Neufile Grotes','arial'],
-        
-      },maxWidth: {
-          '82': '82rem',
-        },
+      fontFamily: {
+        sans: ["Neufile Grotes", "arial"],
+      },
+      maxWidth: {
+        82: "82rem",
+      },
+      margin:{
+        18: "4.4375rem",
+      },
+      padding:{
+        4.5:"1.1219rem"
+      }
     },
     letterSpacing: {
       thin: '-0.015em',
