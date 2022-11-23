5bb0ac33ca06feb524d32d37e10b4bddb155404b
Fidesnoella <fniragena@gmail.com>
2022-11-23 20:25:46 +0200

    refactor:removing break and changing div to p

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -3,24 +3,22 @@ import Wrapper from '../wrappers/Wrapper';
 import Button from '../../components/ui/Button'
 import Link from 'next/link';
 
-function Footer(props) {
+function Footer() {
     return (
         <Wrapper>
            <div className='flex flex-col sm:flex-row justify-between gap-y-12 sm:gap-y-0 text-xl md:text-2xl font-normal leading-8 tracking-thiner md:tracking-thin'>
-            <div>
-                <ul>
-                    <li>Ape Unit GmbH</li>
-                    <li>Waldemarstraße 38, <br /> 10999 Berlin </li>
-                </ul>
-            </div>
+            <p className='flex flex-col'>
+                <span>Ape Unit GmbH</span>
+                <span className='w-56'>Waldemarstraße 38, 10999 Berlin </span>
+            </p>
             <div>
                 <Button link={"https://twitter.com/apeunit"} >Twitter</Button>
                 <Button link={"https://www.facebook.com/apeunit/"} >Facebook</Button>
                 <Button link={"https://www.linkedin.com/company/ape-unit/?originalSubdomain=de"}>LinkedIn</Button>
             </div>
-            <div>
+            <p>
                 <span><Link href={"https://apeunit.com/impressum.txt"} target="_blank">Impressum</Link></span> / <span><Link href={"https://apeunit.com/datenschutz.txt"} target="_blank">Privacy</Link></span>
-            </div>
+            </p>
            </div>
         </Wrapper>
     );
