36e8c97db15a54657612e241b2ea788bdbc88bef
iamzilfa <iamzilfa@gmail.com>
2022-11-23 14:55:40 +0200

    feat: adding header section

--- components/sections/Header.jsx
+++ components/sections/Header.jsx
@@ -1,11 +1,20 @@
-import Wrapper from '../wrappers/Wrapper';
+import Wrapper from "../wrappers/Wrapper";
 
 function Header(props) {
-    return (
-        <Wrapper styles=''>
-      <h1 className="text-8xl font-medium font-sans">Dacade</h1>
- </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <div className="flex flex-col mt-18 font-medium text-xl leading-7">
+        <h1>Ape Unit</h1>
+        <h2 className="pt-4.5">
+          Unit➇ is a pioneering technology and marketing services company that
+          creates end-to-end digital experiences for brands such as Google,
+          Philips, Audi, Twitch, Patagonia, eBay and more. Its team of 2,500+
+          digital specialists across 30+ locations on 5 continents delivers
+          pioneering work on a global scale with a boutique culture.{" "}
+        </h2>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default Header;
