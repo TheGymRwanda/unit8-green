229ee278d5ff1d4ade2e796373b2177b774458bc
Fidesnoella <fniragena@gmail.com>
2022-11-23 12:03:18 +0200

    feat/Adding children props

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 import Wrapper from '../wrappers/Wrapper';
 import arrow from '../../public/assets/images/arrow.svg'
 
-function Button({text, style, link}) {
+function Button({children, style, link}) {
     return (
         <Wrapper>
             <div>
                 <Link href={`${link}`}>
                 <div className='flex gap-1 items-center group cursor-pointer'>
-                <span className={`text-2xl font-normal leading-8 tracking-thin font-sans ${style}`}>{text}</span>
+                <span className={`text-2xl font-normal leading-8 tracking-thin font-sans ${style}`}>{children}</span>
                 <span className='group-hover:-translate-y-1 transition-transform duration-300 group-hover:translate-x-1'><Image src={arrow} /></span>
                 </div>
                 
