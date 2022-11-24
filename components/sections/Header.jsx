d598497eabe45c2a4567f0a0f4299fe73946e507
iamzilfa <iamzilfa@gmail.com>
2022-11-24 19:20:53 +0200

    feat/working on heading

--- components/sections/Header.jsx
+++ components/sections/Header.jsx
@@ -4,12 +4,12 @@ import Button from "../ui/Button";
 function Header() {
   return (
     <Wrapper>
-      <div className="flex flex-col mt-12 md:flex-row  md:gap-x-13 lg:gap-x-81">
-        <h1 className="sm:text-2xl sm:leading-8 sm:tracking-thin font-medium text-xl leading-7 tracking-thiner min-w-[100px]">
+      <div className="grid mt-12 tablet:grid-cols-[250px_minmax(0,_1fr)] desktop:grid-cols-3">
+        <h1 className="text-xl ipad:text-2xl  desktop:col-span-1 mobile:pb-4.5 leading-7 ipad:leading-8 tracking-thiner ipad:tracking-thin font-normal">
           Ape Unit
         </h1>
-        <div className="hidden sm:block">
-          <h2 className="font-medium sm:text-2xl sm:leading-8 sm:tracking-thin sm:pt-4.5 md:pt-0">
+        <div className="hidden ipad:block desktop:col-span-2">
+          <h2 className="font-normal text-2xl leading-8 tracking-thin ipad:pt-0 ipad:block tablet:inline-block">
             Unit➇ is a pioneering technology company specialing in decentralised
             technologies that creates end-to-end digital experiences for
             protocols including{" "}
@@ -19,7 +19,7 @@ function Header() {
             and more. Its team of 2,500+ digital specialists across 30+
             locations on 5 continents delivers pioneering work on a global scale
             with a boutique culture.{" "}
-            <span className="sm:inline-block md:block lg:inline-block">
+            <span className="block tablet:inline-block">
               <Button
                 style={"text-2xl underline hover:no-underline"}
                 link={"mailto:web3@apeunit.com"}
@@ -29,8 +29,8 @@ function Header() {
             </span>
           </h2>
         </div>
-        <div className="block sm:hidden">
-          <h2 className="font-medium text-xl leading-7 tracking-thiner pt-4.5">
+        <div className="block ipad:hidden">
+          <h2 className="font-normal text-xl leading-7 tracking-thiner">
             Unit➇ is a pioneering technology and marketing services company that
             creates end-to-end digital experiences for brands such{" "}
             <span className="text-purple">
@@ -40,7 +40,7 @@ function Header() {
             locations on 5 continents delivers pioneering work on a global scale
             with a boutique culture.
           </h2>
-          <div className="pt-0.0894rem">
+          <div>
             <Button style={"text-xl"} link={"mailto:web3@apeunit.com"}>
               Contact Us
             </Button>
