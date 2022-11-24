89d6573d76f0efb8db914c52256dced32390d8f4
Ngabonziza Nestor <ngabonest@gmail.com>
2022-11-24 15:19:14 +0200

    feat: adding selection background color

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/sections/Header";
 import Footer from "../components/layouts/Footer";
+import DescriptionSection from "../components/sections/DescriptionSection";
 
 export default function Home() {
   return (
@@ -11,8 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Header />
-        <Footer />
+      <Header/>
+      <DescriptionSection/>
+      <Footer />
       </div>
     </div>
   );
