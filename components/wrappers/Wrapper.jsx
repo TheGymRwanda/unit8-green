07d2e7f417274f537888a9ee75399109786998e9
Fidesnoella <fniragena@gmail.com>
2022-11-25 10:11:16 +0200

    ft: Done working on expertise section

--- components/wrappers/Wrapper.jsx
+++ components/wrappers/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Wrapper({ children, styles }) {
   return (
-    <div className={`max-w-90 px-5 mobile:px-9 ipad:px-16 mx-auto ${styles}`}>
+    <div className={`max-w-90 mobile:px-5 ipad:px-9 desktop:px-16 mx-auto ${styles}`}>
       {children}
     </div>
   );
