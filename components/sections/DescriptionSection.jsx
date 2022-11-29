047dabbaf3ca4b681199ae3d661701d2a34306f4
esther921 <estherniyonkuru51@gmail.com>
2022-11-29 17:04:15 +0200

    feat:done working on reviews given

--- components/sections/DescriptionSection.jsx
+++ components/sections/DescriptionSection.jsx
@@ -1,37 +1,37 @@
-import React, { useState, useEffect, useRef } from "react";
-import { createRef } from "react";
-import Wrapper from "../wrappers/Wrapper";
-import Arrow from "../Arrow";
-import Button from "../ui/Button";
+import React, {useState, useEffect, useRef} from 'react';
+import {createRef} from 'react';
+import Wrapper from '../wrappers/Wrapper';
+import Arrow from '../Arrow';
+import Button from '../ui/Button';
 
 function DescriptionSection() {
   const desc = [
     {
-      header: "About us",
+      header: 'About us',
       description:
-        "Bloc about us will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale",
-      link: "/link",
-      end: "Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.",
+        'Bloc about us will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale. ',
+      link: '/link',
+      end: 'Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.',
     },
     {
-      header: "What we do",
+      header: 'What we do',
       description:
-        "Bloc wat we do will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale",
-      link: "/link",
-      end: "Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.",
+        'Bloc wat we do will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale. ',
+      link: '/link',
+      end: 'Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.',
     },
     {
-      header: "Employment",
+      header: 'Employment',
       description:
-        "Bloc employment will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale",
-      link: "/link",
-      end: "Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.",
+        'Bloc employment will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale. ',
+      link: '/link',
+      end: 'Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.',
     },
   ];
   const [description, setDescription] = useState(0);
   const [show, setShow] = useState(true);
   const refs = useRef(new Array());
-  const [position, setPosition] = useState({ width: 0, left: 0 });
+  const [position, setPosition] = useState({width: 0, left: 0});
   useEffect(() => {
     setShow(true);
     setPosition({
@@ -52,33 +52,36 @@ function DescriptionSection() {
                 onClick={() => setDescription(i)}
                 className={`${
                   description === i
-                    ? "text-blue-600 one"
-                    : "text-secondary-100 no-underline"
+                    ? 'text-purple one'
+                    : 'text-secondary-100 no-underline'
                 }`}
                 ref={(element) => (refs.current[i] = element)}
               >
                 {item.header}
+                <hr
+                  style={{
+                    width: `${position.width}rem`,
+                    left: `${position.left}rem`,
+                  }}
+                  className={`absolute top-[100%] h-[.15rem] inline-block bg-purple transition-all duration-150 ease-in-out`}
+                />
               </button>
             ))}
-
-            <hr
-              style={{
-                width: `${position.width}rem`,
-                left: `${position.left}rem`,
-              }}
-              className={`absolute top-[100%] h-[.15rem] inline-block bg-blue-600 transition-all duration-200 ease-in-out`}
-            />
           </div>
-          <div className="my-6">
-            <p>{desc[description].description}</p>
-            <Button style="underline hover:no-underline hover:duration-300 ease-in-out">
-              Learn more
-            </Button>
+          <div className="my-6 relative ">
+            <div>
+              {desc[description].description}
+              <span className="block tablet:inline-block">
+                <Button style="underline hover:no-underline hover:duration-300 ease-in-out inline-block">
+                  Learn more
+                </Button>
+              </span>
+            </div>
             <p className="my-6">{desc[description].end}</p>
           </div>
         </div>
       </div>
-      <div className="flex flex-col ipad:hidden gap-8 justify-start w-full ">
+      <div className="flex flex-col ipad:hidden gap-4 justify-start w-full ">
         {desc.map((item, i) => (
           <div key={i} className="">
             <button
@@ -88,8 +91,8 @@ function DescriptionSection() {
               }}
               className={`${
                 description === i && show
-                  ? " text-blue-600 border-b border-blue-500 py-1 stroke-blue-500"
-                  : "text-secondary-100 border-b border-black stroke-black"
+                  ? ' text-purple border-b border-purple py-1 stroke-purple'
+                  : 'text-black border-b border-black stroke-black'
               } flex justify-between  w-full`}
             >
               <span className="whitespace-nowrap font-normal text-2xl">
@@ -98,31 +101,32 @@ function DescriptionSection() {
 
               <div
                 className={`${
-                  description === i && show && "rotate-180 "
+                  description === i && show && 'rotate-180 '
                 } duration-500 ease-in-out`}
               >
                 <Arrow />
               </div>
             </button>
             {description === i && show && (
-              <div className={`overflow-hidden`}>
+              <div className={`overflow-hidden pt-3`}>
                 <p
-                  className={`animate-dropdown ${
-                    show === "false" ? " animate-dropup" : ""
+                  className={` ${
+                    description === i && show
+                      ? ' animate-dropdown'
+                      : ' animate-dropup'
                   }`}
                 >
                   {item.description}
+                  <span className="inline-block">
+                    <button>Learn more</button>
+                  </span>
                 </p>
-                <button
-                  className={`animate-dropdown ${
-                    show === "false" ? " animate-dropup" : ""
-                  }`}
-                >
-                  Learn more
-                </button>
+
                 <p
-                  className={`animate-dropdown ${
-                    show === "false" ? " animate-dropup" : ""
+                  className={`animate-dropdown pt-5 ${
+                    description === i && show
+                      ? ' animate-dropdown'
+                      : ' animate-dropup'
                   }`}
                 >
                   {item.end}
