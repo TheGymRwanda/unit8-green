71552739a07946b16bf52af55c8f32fa8abe1668
esther921 <estherniyonkuru51@gmail.com>
2022-12-01 21:13:45 +0200

    feat:solved merge conflicts

--- components/sections/Expertise.jsx
+++ components/sections/Expertise.jsx
@@ -2,43 +2,6 @@ import React from "react";
 import Wrapper from "../wrappers/Wrapper";
 
 function Expertise(props) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    const tagOne = ["Product Development", "Community management", "Governance", "Product Development", "Product Development"]
-    const tagTwo = ["Product Design", "Strategy", "Product Design", "Product Design", "Product Design"]
-     return (
-        <Wrapper>
-            <div className="w-full grid grid-cols-3 place-items-end">
-            <div />
-            <div className='col-span-2 grid gap-y-6'>
-            <h3 className='text-secondary-100 text-xl md:text-2xl leading-8 tracking-thin self-start'>Expertise</h3>
-            <div className='flex w-full flex-col ipad:flex-row gap-y-3 ipad:gap-x-6 self-end'>
-                <ul className='flex flex-col gap-y-3 ipad:gap-y-4'>
-                    {
-                        tagOne.map((tag, i) => {
-                            return (
-                                    <li key={i} className ="border-b border-secondary-50 leading-8 max-w-full whitespace-nowrap ipad:w-78 text-xl font-normal ipad:text-2xl tracking-thiner ipad:tracking-thin">{tag}</li>
-                            )
-                        })
-                    }
-                </ul>
-                <ul className='flex flex-col gap-y-3 ipad:gap-y-4'>
-                    {
-                        tagTwo.map((tag, i) => {
-                           return (
-                            <li key={i} className ="border-b border-secondary-50 max-w-full whitespace-nowrap ipad:w-78 text-xl leading-8 ipad:text-2xl tracking-thiner ipad:tracking-thin font-normal">{tag}</li>
-                           )
-                         })
-                    }
-                </ul>
-            </div>
-            </div>  
-          </div>            
-        </Wrapper>
-    );
-=======
-=======
->>>>>>> 2713d1b67bf885cdbd46d0bf64d98c7973eda47a
   const tagOne = [
     "Product Development",
     "Community management",
@@ -91,10 +54,6 @@ function Expertise(props) {
       </div>
     </Wrapper>
   );
-<<<<<<< HEAD
->>>>>>> 61c22c6 (feat:fixed the rest of the breakpoints)
-=======
->>>>>>> 2713d1b67bf885cdbd46d0bf64d98c7973eda47a
 }
 
 export default Expertise;
