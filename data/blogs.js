c2af786cad389834747f85bcc12fa83ddc391534
Ngabonziza Nestor <ngabonest@gmail.com>
2022-12-14 18:29:43 +0200

    feat: blog

--- data/blogs.js
+++ data/blogs.js
@@ -77,5 +77,35 @@ const BLOGS = [
     paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
     tags: ['Blockchain','DAOs','Decentralization']
 },
+{
+    date:'02 Oct 2022',
+    title: 'Launch of the Africa DeFi Alliance DAO',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    tags: ['Blockchain','DAOs','Decentralization']
+},
+{
+    date:'02 Oct 2022',
+    title: 'Launch of the Africa DeFi Alliance DAO',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    tags: ['Blockchain','DAOs','Decentralization']
+},
+{
+    date:'02 Oct 2022',
+    title: 'Launch of the Africa DeFi Alliance DAO',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    tags: ['Blockchain','DAOs','Decentralization']
+},
+{
+    date:'02 Oct 2022',
+    title: 'Launch of the Africa DeFi Alliance DAO',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    tags: ['Blockchain','DAOs','Decentralization']
+},
+{
+    date:'02 Oct 2022',
+    title: 'Launch of the Africa DeFi Alliance DAO',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    tags: ['Blockchain','DAOs','Decentralization']
+},
 ]
 export default BLOGS;
