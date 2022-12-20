83152bce0821c006270da2440183640fa0669d1c
Fidesnoella <fniragena@gmail.com>
2022-12-21 00:48:03 +0200

    feat: adding styles on blog card component

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-function Card({date,title, paragraph,tags}) {
-    return (
-        <div className='rounded-md max-w-[300px]'>
-          <p className='text-base text-gray-400'>{date}</p>  
-          <h1 className='text-4xl font-semibold'>{title}</h1>
-          <p className='text-xl'>{paragraph}</p>
-          <div className='flex gap-4 mt-4'>
-          {tags.map((tag, index)=><p key={index} className='bg-gray-200 text-gray-500 px-1 rounded-full text-base'>{tag}</p>)}
-          </div>
-        </div>
-    );
+const Card = ({ date, title, paragraph, tags }) => {
+  return (
+    <div className='rounded-md max-w-[302px] ipad:max-w-[477px] min-w-[302px] ipad:min-w-[477px] max-h-[410px] ipad:max-h-[401.39px] min-h-[410px] ipad:min-h-[401.39px] border border-[#EDEDED] p-5 shadow-md'>
+      <p className='text-gray-400 text-lg font-normal pb-6'>{date}</p>
+      <h1 className='text-[28px] ipad:text-4xl pb-3'>{title}</h1>
+      <p className='text-xl ipad:text-2xl mb-6 ipad:mb-[38px]'>{paragraph}</p>
+      <div className='flex gap-x-6 flex-wrap'>
+        {tags.map((tag, index) => <p key={index} className='bg-gray-200 text-gray-400 px-1 rounded-full text-base'>{tag}</p>)}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
