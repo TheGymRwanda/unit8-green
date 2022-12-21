2e727b02ca9065b5914d18e363cf16f7be1d6545
esther921 <estherniyonkuru51@gmail.com>
2022-12-21 11:26:39 +0200

    fix:maintained the alignment of the section images

--- components/sections/ProjectSection.jsx
+++ components/sections/ProjectSection.jsx
@@ -15,14 +15,14 @@ export default function ProjectSections() {
 
   return (
     <Wrapper
-      styles={`w-full desktop:mt-[8.5rem] desktop: mb-[7.9375rem] tablet:mb-[6.5888rem] tablet:mt-[6.625rem] ipad:mb-[5.7375rem] ipad:mt-[4.1744rem] mobile:mb-[5.3125rem] mobile:mt-[2.0625rem]`}
+      styles={`w-full desktop:mt-34 desktop: mb-31 tablet:mb-26.5 tablet:mt-27 ipad:mb-23 ipad:mt-17.2 mobile:mb-20.3 mobile:mt-7.06`}
     >
-      <p className="text-secondary-100 text-2xl desktop:-space-y-6 desktop:mb-0 desktop:pl-1 mt-16 mb-4 ipad:mt-16 desktop:mt-0 leading-[1.75rem]">
+      <p className="text-secondary-100 text-2xl desktop:-space-y-6 desktop:mb-0 desktop:pl-1 mt-16 mb-4 ipad:mt-16 desktop:mt-0 leading-7">
         Project
       </p>
 
-      <div className="hidden desktop:grid desktop:grid-cols-4 relative mt-[8.5px]">
-        <ul className="flex flex-col text-[86px] cursor-pointer -space-y-5 leading-snug">
+      <div className="hidden desktop:grid desktop:grid-cols-5 relative mt-2.25">
+        <ul className="flex flex-col text-7.5xl cursor-pointer col-span-2 -space-y-5 leading-snug">
           {IMAGES.map(
             (item, i) =>
               i !== 0 && (
@@ -31,7 +31,7 @@ export default function ProjectSections() {
                   className={`flex flex-col overflow-hidden desktop:inline-block mb-0`}
                 >
                   <div
-                    className={` inline-block relative group desktop:after:content-[''] desktop:after:absolute desktop:after:h-[0.125rem] desktop:after:w-0 desktop:after:bottom-5 desktop:after:hover:w-full desktop:after:left-0 desktop:after:origin-left desktop:after:ml-1 desktop:after:bg-black desktop:hover:after:origin-left desktop:after:transition-all desktop:after:duration-200 desktop:after:ease-in-out desktop:after:delay-100`}
+                    className={` inline-block relative group desktop:after:content-[''] desktop:after:absolute desktop:after:h-0.5 desktop:after:w-0 desktop:after:bottom-5 desktop:after:hover:w-full desktop:after:left-0 desktop:after:origin-left desktop:after:ml-1 desktop:after:bg-black desktop:hover:after:origin-left desktop:after:transition-all desktop:after:duration-200 desktop:after:ease-in-out desktop:after:delay-100`}
                     onMouseEnter={() => setLink(i)}
                     onMouseLeave={() => setLink(i)}
                   >
@@ -54,9 +54,9 @@ export default function ProjectSections() {
           )}
         </ul>
         {link !== 0 && (
-          <div className="sticky h-fit top-4 col-span-3 mt-10">
+          <div className="sticky h-fit top-4 mt-10 col-span-3">
             <Image
-              className="rounded-xl shadow-pictures desktop:inline-block max-w-fit max-h-fit"
+              className="rounded-xl shadow-pictures desktop:inline-block max-w-full max-h-full"
               src={IMAGES[link].src}
               alt=""
               width={1000}
@@ -81,17 +81,18 @@ export default function ProjectSections() {
                 >
                   {item.heading}
                   {link === i && show && (
-                    <div className="tablet:mt-[11.6px] ipad:mt-[]">
+                    //
+                    <div className="tablet:mt-2.7 ipad:mt-[]">
                       <Link href={IMAGES[link].link} target={"_blank"}>
                         <Image
                           src={item.src}
                           alt=""
                           width="1000"
                           height="1000"
-                          className="rounded-xl shadow-pictures mt-3 mb-12 tablet:mt-5 tablet:min-w-[59.5rem] tablet:min-h-[32.5613rem] ipad:mb-16 ipad:min-w-[47.4769rem] ipad:h-[25.9831rem] min-w-[21.8125rem] min-h-[11.9375rem]"
+                          className="rounded-xl shadow-pictures mt-3 mb-12 tablet:mt-5 tablet:min-w-59.5 tablet:min-h-32.5 ipad:mb-16 ipad:min-w-47.4 ipad:h-97.9 min-w-21.8 min-h-11.9"
                         />
                       </Link>
-                      <p className="text-xl tracking-tight tablet:mt-[0.8013rem] tablet:mb-[3.7875rem] max-w-[51.5rem] ipad:mt-[0.5581rem] ipad:mb-[60.07px]">
+                      <p className="text-xl tracking-tight tablet:mt-3.8 tablet:mb-15.78 max-w-51.5 ipad:mt-2.55 ipad:mb-15.7">
                         {IMAGES[link].quote}
                       </p>
                     </div>
