4b9bbdaa1b4a4805a97bf0a3477fb1124d87f1a1
Ngabonziza Nestor <ngabonest@gmail.com>
2022-12-21 12:37:13 +0200

    fix:fixing height issue on mobile

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Card = ({ date, title, paragraph, tags }) => {
   return (
-    <div className='rounded-md max-w-[302px] ipad:max-w-[477px] min-w-[302px] ipad:min-w-[477px] max-h-[410px] ipad:max-h-[401.39px] min-h-[410px] ipad:min-h-[401.39px] border border-[#EDEDED] p-5 shadow-md'>
+    <div className='rounded-md max-w-[302px] ipad:max-w-[477px] min-w-[302px] ipad:min-w-[477px] ipad:max-h-[401.39px] min-h-[410px] ipad:min-h-[401.39px] border border-[#EDEDED] p-5 shadow-md'>
       <p className='text-gray-400 text-lg font-normal pb-6'>{date}</p>
       <h1 className='text-[28px] ipad:text-4xl pb-3'>{title}</h1>
       <p className='text-xl ipad:text-2xl mb-6 ipad:mb-[38px]'>{paragraph}</p>
-      <div className='flex gap-x-6 flex-wrap'>
+      <div className='flex gap-x-6 gap-y-4 flex-wrap'>
         {tags.map((tag, index) => <p key={index} className='bg-gray-200 text-gray-400 px-1 rounded-full text-base'>{tag}</p>)}
       </div>
     </div>
